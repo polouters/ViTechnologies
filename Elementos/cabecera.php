17c2d7f93cb8fef7964e29e4c41bf9a42fbaa80c
Ruben Polo <polouters@gmail.com>
2016-06-06 16:03:58 +0200

    Plantilla Base V1.0 Funcional, Nombre de la plantilla ViTechnologies

---
+++ Elementos/cabecera.php
@@ -0,0 +1,10 @@
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="intro-text">
+            <div class="intro-lead-in">Bienvenido a ViTechnologies</div>
+            <div class="intro-heading">La frase de mikel</div>
+            <a href="#services" class="page-scroll btn btn-xl">Leer más</a>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once __DIR__."/Elementos/head.php";
+require_once __DIR__."/Elementos/nav.php";
+require_once __DIR__."/Elementos/cabecera.php";
+require_once __DIR__."/Elementos/servicios.php";
+
+
+
+require_once __DIR__."/Elementos/footer.php";
+
+
+?>
